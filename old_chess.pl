cb115296ab9cfef838a8f1c645abe2389a34980d
Ben Rickman <rickman.ben@gmail.com>
2015-03-25 14:34:55 -0500

    added an optional different coloring style to the pieces. fixed the piece selection request so that it works again.

--- old_chess.pl
+++ old_chess.pl
@@ -5,11 +5,10 @@ use strict;
 use v5.10;
 use Term::ANSIColor;
 
-my @board;
-my @wpieces = qw(♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖);
-@wpieces = qw(wr wn wb wq wk wb wn wr);
-my @bpieces = qw(♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜);
-@bpieces = qw(br bn bb bq bk bb bn br);
+my $debug_1 = 0;
+my $board;
+my @wpieces = qw(wr wn wb wq wk wb wn wr);
+my @bpieces = qw(br bn bb bq bk bb bn br);
 my $pieces = join '', (@wpieces,@bpieces);
 my $chess_unicode = {
   k => [qw(♔ ♚)],
@@ -20,44 +19,57 @@ my $chess_unicode = {
   p => [qw(♙ ♟)],
 };
 
-my %letters = (
-  A => 1,
-  B => 2,
-  C => 3,
-  D => 4,
-  E => 5,
-  F => 6,
-  G => 7,
-  H => 8,
-);
+sub letters {
+  my $i = shift;
+  $i =~ tr/a-h/A-H/;
+  if ($i =~ /\w/) {
+    $i =~ tr/A-H/1-8/;
+  } else {
+  $i =~ tr/1-8/A-H/;
+  }
+  return $i
+}
 
 for my $column (1..8){
-  $board[8][$column] = "$bpieces[$column-1]";
-  $board[7][$column] = "bp";
-  $board[1][$column] = "$wpieces[$column-1]";
-  $board[2][$column] = "wp";
-  $board[0][$column] = " $column ";
+  $board->[8][$column] = "$bpieces[$column-1]";
+  $board->[7][$column] = "bp";
+  $board->[1][$column] = "$wpieces[$column-1]";
+  $board->[2][$column] = "wp";
+  $board->[0][$column] = " $column ";
 }
-for (keys %letters) {
-  $board[$letters{$_}][0]="  $_   ";
+for (1..8) {
+  $board->[$_][0]="  " . letters($_) . "   ";
 } 
-$board[0][0]="^y, x>";
+$board->[0][0]="^y, x>";
 while (1) {
   for my $row (reverse(0..8)) {
     for my $column (0..8) {
     my $unicode;
-    if ((defined $board[$row][$column]) and !($board[$row][$column] =~ /[$pieces]/)) {
-      print "$board[$row][$column]";
-    } elsif ((defined $board[$row][$column]) and !(($row+$column)%2)) {
-      if ($board[$row][$column] =~ /(b|w)+(p|k|q|b|n|r)+/) {
-        $unicode = $1 eq 'b' ? $chess_unicode->{$2}[1] : $chess_unicode->{$2}[0];
-    }
-      print colored(" $unicode ", 'white on_black');
-    } elsif ((defined $board[$row][$column])) {
-      if ($board[$row][$column] =~ /(b|w)(p|k|q|b|n|r)/) {
-        $unicode = $1 eq 'b' ? $chess_unicode->{$2}[0] : $chess_unicode->{$2}[1];
-    }
-      print colored(" $unicode ", 'black on_white');
+    my $color;
+    if ((defined $board->[$row][$column]) and !($board->[$row][$column] =~ /[$pieces]/)) {
+      print "$board->[$row][$column]";
+    } elsif ((defined $board->[$row][$column]) and !(($row+$column)%2)) {
+      if ($board->[$row][$column] =~ /(b|w)(p|k|q|b|n|r)/) {
+        if ($debug_1) {
+          $unicode = $1 eq 'b' ? $chess_unicode->{$2}[0] : $chess_unicode->{$2}[1];
+          $color = 'white';
+	} else {
+          $unicode = $chess_unicode->{$2}[1];
+          $color   = $1 eq 'b' ? 'red' : 'yellow';
+        }
+      }
+      print colored(" $unicode ", "$color on_black");
+    } elsif ((defined $board->[$row][$column])) {
+      if ($board->[$row][$column] =~ /(b|w)(p|k|q|b|n|r)/) {
+        if ($debug_1) {
+          $unicode = $1 eq 'b' ? $chess_unicode->{$2}[1] : $chess_unicode->{$2}[0];
+          $color = 'black';
+	} else {
+          $unicode = $chess_unicode->{$2}[1];
+          $color   = $1 eq 'b' ? 'red' : 'yellow';
+        }
+      }
+      print colored(" $unicode ", "$color on_white");
     } elsif (($row+$column)%2){
       print colored("   ", 'white on_white');
     } else {
@@ -66,21 +78,26 @@ while (1) {
   } print "\n";
   }
 
+  $board->[0][0] = undef;
   print "\nNext move?\n\n";
-  my $move = <>;
-  last unless ($move =~ /([a-h]),\s?([1-8])/);
-  my $starty = $letters{$1};
-  my $startx = $3;
-  unless (defined $board[$starty][$startx]) {
+  my ($startx,$starty) = get_space();
+  until (defined $board->[$starty][$startx]) {
     print "\nThere is no piece there\n\n";
-    next
+    ($startx,$starty) = get_space();
   }
-  print "\nWhere will $board[$starty][$startx] go?\n\n"; 
-  $move = <>;
+  print "\nWhere will $board->[$starty][$startx] go?\n\n"; 
+  my ($endx,$endy) = get_space();
   print "\n";
-  last unless ($move =~ /([a-h]),(\s)?([1-8])/);
-  my $endy = $letters{$1};
-  my $endx = $3;
-  $board[$endy][$endx]=$board[$starty][$startx];
-  $board[$starty][$startx] = undef;
+  $board->[$endy][$endx]=$board->[$starty][$startx];
+  $board->[$starty][$startx] = undef;
 }  
+
+sub get_space {
+  my $move = <>;
+  until ($move =~ /([A-Ha-h]),?\s?([1-8])/) {
+    print "\nBad format, try again\n\n";
+    $move = <>;
+  }
+  my ($sy,$sx) = $move =~ /([A-Ha-h]),?\s?([1-8])/;
+  return ($sx,letters($sy))
+}
