2c263842949a3fbb0cf53c9ef158cbe7592ee943
Ben Rickman <rickman.ben@gmail.com>
2015-03-24 22:07:44 -0500

    starting draw mode in Game

--- Game.pm
+++ Game.pm
@@ -1,8 +1,13 @@
 package Game;
 use Moose;
-
+use Term::ANSIColor;
 use Board;
 use Piece;
 
-has 'board' => ( is 'ro', isa 'Board', );
+#has 'board' => ( is 'ro', isa 'Board', );
 
+
+print color 'bold blue';
+print "This text is bold blue.\n";
+print color 'reset';
+print "This text is normal.\n";
