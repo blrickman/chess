8ec98dfba9610720b94878c38f9b9216786aa7ed
Ben Rickman <rickman.ben@gmail.com>
2015-03-26 00:37:47 -0500

    full board setup

--- chess.pl
+++ chess.pl
@@ -5,16 +5,32 @@ use Pieces;
 use Game;
 
 my $board = Board->new();
-my $piece1 = Rook->new(posx => 1, posy => 1, board => $board);
-my $piece2 = Rook->new(posx => 8, posy => 1, board => $board, white => 1);
-my $piece3 = King->new(posx => 5, posy => 1, board => $board);
-$board->add_pieces([$piece1,$piece2,$piece3]);
+my (@p1,@p2,@p7,@p8);
 
-print "{";
-for ( @{$piece3->pot_moves()}) {
-  print "{" . join(', ',@{$_})  . "},";
-};
-print "\b}\n";
+$p1[0] = Rook->new(   posx => 1, posy => 1, board => $board);
+$p1[1] = Knight->new( posx => 2, posy => 1, board => $board);
+$p1[2] = Bishop->new( posx => 3, posy => 1, board => $board);
+$p1[3] = Queen->new(  posx => 4, posy => 1, board => $board);
+$p1[4] = King->new(   posx => 5, posy => 1, board => $board);
+$p1[5] = Bishop->new( posx => 6, posy => 1, board => $board);
+$p1[6] = Knight->new( posx => 7, posy => 1, board => $board);
+$p1[7] = Rook->new(   posx => 8, posy => 1, board => $board);
+$p2[$_]= Pawn->new(   posx => $_+1,posy => 2, board => $board) for (0..7);
+
+$p8[0] = Rook->new(   posx => 1, posy => 8, board => $board, white => 0);
+$p8[1] = Knight->new( posx => 2, posy => 8, board => $board, white => 0);
+$p8[2] = Bishop->new( posx => 3, posy => 8, board => $board, white => 0);
+$p8[3] = Queen->new(  posx => 4, posy => 8, board => $board, white => 0);
+$p8[4] = King->new(   posx => 5, posy => 8, board => $board, white => 0);
+$p8[5] = Bishop->new( posx => 6, posy => 8, board => $board, white => 0);
+$p8[6] = Knight->new( posx => 7, posy => 8, board => $board, white => 0);
+$p8[7] = Rook->new(   posx => 8, posy => 8, board => $board, white => 0);
+$p7[$_]= Pawn->new(   posx => $_+1,posy => 7, board => $board, white => 0) for (0..7);
+
+my @pieces = (@p1,@p2,@p7,@p8);
+$board->add_pieces(\@pieces);
+
+$p8[5]->move_to(2,1)
 
 
 __END__
